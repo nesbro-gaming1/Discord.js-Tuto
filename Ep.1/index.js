812ca6ccd19b567c6d8b0d8f0e84831882df850c
NESBEERAS <76590320+NESBEERAS@users.noreply.github.com>
2021-02-12 12:29:18 +0530

    Update index.js

--- Ep.1/index.js
+++ Ep.1/index.js
@@ -1,28 +1,17 @@
-const express = require('express');
-const discord = require('discord.js');
-const config = require('./config.json');
-const { token, prefix } = require('./config');
-const client = new discord.Client();
+const config = require("./config.json");
+const { token, prefix } = require("./config");
+const discord = require("discord.js");
+const client = new discord.Client()
 
-client.on('ready', () => {
-  console.log(`${client.user.username}`);
-  client.user.setActivity('DISCORD.JS', {type: 'watching'});
+client.on("ready", () => {
+     console.log(`${client.user.tag} is ready`)
+     client.user.setActivity("YOUR STATUS!")
 });
 
-client.on('message', message => {
-  if (message.content === `${prefix}ping`) {
-    return message.channel.send(`🏓pong! ${(client.ws.ping)}-ms`);
-  }
-});
+client.on("message", message => {
+   if (message.content === `${prefix}ping`) {
+   return message.channel.send("Pong!")
+}
+})
 
 client.login(token);
-
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hello Express app!')
-});
-
-app.listen(3000, () => {
-  console.log('server started');
-});
